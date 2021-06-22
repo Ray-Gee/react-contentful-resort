5e76d4218af8001698d09e0c067b18866ff5069e
Ryuichi-g <hsatlefp@gmail.com>
2021-06-22 18:40:03 +0900

    modified

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-useless-constructor */
 import React, { Component } from "react";
 import defaultBcg from "../images/room-1.jpeg";
-import Hero from "../components/Hero";
+// import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import { RoomContext } from "../context";
